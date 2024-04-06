9b2b8151dc46c2dfd1fa23f7a413d507e61a3c94
Leon <leon.muriuki@student.moringaschool.com>
2024-04-06 22:37:56 +0300

    Added imports in app.py

---
+++ server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db, User, Recipe, FavoriteRecipe
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'your_secret_key_here'  
+    CORS(app)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
