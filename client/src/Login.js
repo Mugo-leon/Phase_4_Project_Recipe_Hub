7d371ba15bb508bec9b7248e99bd25ff4e1ef62e
Janemwaura <mwaura461@gmail.com>
2024-09-02 21:44:33 +0300

    re-installed gunicorn

--- client/src/Login.js
+++ client/src/Login.js
@@ -18,7 +18,7 @@ function Login({ onLogin }) {
         };
 
         try {
-            const response = await fetch('https://sevenphase-4-project-recipe-hub-7.onrender.com/login', {
+            const response = await fetch('https:/sevenphase-4-project-recipe-hub-7.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
