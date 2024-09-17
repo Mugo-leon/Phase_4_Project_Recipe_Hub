17e6ecccf652af9787535b60bbc647d3044615cf
Janemwaura <mwaura461@gmail.com>
2024-09-17 20:40:29 +0300

    updated cors policy line

--- server/app.py
+++ server/app.py
@@ -26,7 +26,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     Session(app)
-    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://nine-project-recipe-hub.onrender.com"}}) 
+    CORS(app, resources={r"/*": {"origins": "https://nine-project-recipe-hub.onrender.com"}}, supports_credentials=True)
 
     return app
 
